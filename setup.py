1b4667f12f923c27eb9639c537fd7fb0cbad56cd
utkarsh1711 <72968732+utkarsh1711@users.noreply.github.com>
2022-09-12 14:34:01 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,5 +6,6 @@ setup(name='ennoml',
     author='Utkarsh',
     author_email='author@somemail.com',
     license='MIT',
-    packages=['.ennoml','.ennoml.data','.ennoml.testing','.ennoml.training','.ennoml.util','.ennoml.models'])
+    packages=['.ennoml','.ennoml.data','.ennoml.testing','.ennoml.training','.ennoml.util','.ennoml.models'],
+    install_requires=['tensorflow-gpu>=2.3,<2.7','pandas','Pillow','opencv-python','numpy', 'scikit-image','xlswriter'])
     # zip_safe=False)
