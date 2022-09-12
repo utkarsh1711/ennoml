a062dc6c97f6d1088ed3d87fc88bceb4713c2d3f
utkarsh1711 <72968732+utkarsh1711@users.noreply.github.com>
2022-09-12 14:27:46 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='ennoml',
+    version='0.1',
+    description='test package to run on qbee.io',
+    author='Utkarsh',
+    author_email='author@somemail.com',
+    license='MIT',
+    packages=['.ennoml','.ennoml.data','.ennoml.testing','.ennoml.training','.ennoml.util','.ennoml.models'])
+    # zip_safe=False)
